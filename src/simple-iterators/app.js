560a0b76edaad4f26c6c552e27bbea3fea142abb
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-09 21:08:02 -0300

    Finished simple iterators module :mortar_board:

---
+++ src/simple-iterators/app.js
@@ -0,0 +1,17 @@
+const students = ['Marcos', 'Marina', 'João'].reduce((allStudents, name) => {
+    allStudents.push({ name, id: allStudents.length });
+    return allStudents;
+}, []);
+
+let iterator = students[Symbol.iterator]();
+console.log(`First value`, iterator.next());
+
+let iteratorSelector = students[Symbol.iterator]();
+let done = false;
+let next = iterator.next();
+do {
+    let student = next.value;
+    console.log('Select current', student);
+    next = iterator.next();
+} while (!next.done);
+
