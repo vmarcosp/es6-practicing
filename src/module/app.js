ddeeb0dd30aa73b5f35fce1f87389ea26254ee32
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-09 21:01:45 -0300

    :sparkles: Initialized module of iterators

--- src/module/app.js
+++ src/module/app.js
@@ -1,1 +1,7 @@
-import Container from '../utils/Container';
+const students = ['Marcos', 'Marina', 'João'].reduce((allStudents, name) => {
+    allStudents.push({ name, id: allStudents.length });
+    return allStudents;
+}, []);
+
+let iterator = students[Symbol.iterator]();
+console.log(`First value`,iterator.next());
